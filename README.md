d4447f078732764c151500f5cda7967638702b87
a.barkook <alibarkook@gmail.com>
2021-09-05 19:03:36 +0430

    update -> finish project

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # js-practice
 java script project for interns practice
+
+fetch api
+work with js create html element
--- app.js
+++ app.js
@@ -1,23 +1,17 @@
 searchForUser()
 
 async function searchForUser() {
-    // console.log(e);
-    // fetch('https://reqres.in/api/users?page=1')
-    // .then(function (response) {
-    //     console.log(response);
-    // });
 
     let response = await fetch('https://reqres.in/api/users?page=1');
     let user = await response.json();
 
 
-    console.log(user.data);
 
     const ul = document.getElementById("userList");
+
     user.data.forEach(user => {
         const li = document.createElement("li");
         li.innerText = user.first_name;
         ul.appendChild(li);
     });
 }
-
